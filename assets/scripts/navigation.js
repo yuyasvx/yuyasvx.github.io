872743891455f69ca474adee9bdbe167540d1dce
YUYASVX <yuyasvx@gmail.com>
2015-06-02 04:39:58 +0900

    site

--- assets/scripts/navigation.js
+++ assets/scripts/navigation.js
@@ -1,6 +1,7 @@
 //navigation.js
 
 var isTopPage = true;
+var isNavDisplayed = true;
 
 var navHeight = $("#fixedmenu").height();
 var logobtnHeight = $(".svxlogobox").eq(0).height();
@@ -9,16 +10,35 @@ console.log("logobtnHeight" + logobtnHeight);
 $("#fixedmenu").css("height", navHeight + "px");
 
 var NV_minimize = function(){
-  $("#fixedmenu ul").css("display", "none");
-  $("#fixedmenu").css("overflow", "hidden");
-  $("#fixedmenu").animate({height: "100px"},{duration: 100});
-  $("#fixedmenu #LOGOTYPE").animate({opacity: "0"},{duration: 100});
-  $("#fixedmenu svg").animate({top: "-15px"},{duration: 100});
-  
-  isTopPage = false;
+  if(isTopPage) {
+    $("#fixedmenu ul").css("display", "none");
+    $("#fixedmenu").css("overflow", "hidden");
+    $("#fixedmenu").animate({height: "100px"},{duration: 100});
+    $("#fixedmenu #LOGOTYPE").animate({opacity: "0"},{duration: 100});
+    $("#fixedmenu svg").animate({top: "-28px"},{duration: 100});
+    $("#fixedmenu svg ").css("transform", "scale(0.8,0.8)");
+    isTopPage = false;
+    isNavDisplayed = false;
+  }
 };
 
 var NV_expand = function(){
+  
+  if(!isTopPage) {
+    $("#fixedmenu #LOGOTYPE").animate({opacity: "1"},{duration: 100});
+    $("#fixedmenu svg").animate({top: "0px"},{duration: 100});
+    $("#fixedmenu svg ").css("transform", "scale(1,1)");
+
+    var d = new $.Deferred();
+    $("#fixedmenu").animate({height: navHeight + "px"},{duration: 100});
+    $("#fixedmenu ul").css("display", "block");
+    isTopPage = true;
+    isNavDisplayed = true;
+  }
+  
+  d.resolve();
+  
+  return d.promise();
 };
 
 function adjustNav(){
@@ -26,6 +46,8 @@ function adjustNav(){
   var v_crdt = $(window).scrollTop();
   if (v_crdt >= logobtnHeight){
     NV_minimize();
+  } else if ( v_crdt < logobtnHeight ) {
+    NV_expand();
   }
 }
 
