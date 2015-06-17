d87aa7fcbdda65233ff79ea4567ccf218789c5ce
YUYASVX <yuyasvx@gmail.com>
2015-06-17 21:16:46 +0900

    pages

--- assets/scripts/navigation.js
+++ assets/scripts/navigation.js
@@ -1,20 +1,30 @@
 //navigation.js
 
-var isTopPage = true;
+var isTopPage = false;
 var isNavDisplayed = true;
+var isHome = true;
+if ($('body').data('is-home') == "false" || $('body').data('is-home') == undefined) {
+  isHome = false;
+}
 
 var navHeight = $("#fixedmenu").height();
 var logobtnHeight = $(".svxlogobox").eq(0).height();
 
 $("#fixedmenu").css("height", navHeight + "px");
 
-var NV_minimize = function(){
+var NV_minimize = function(isAnimated){
   $("#fixedmenu ul").css("display", "none");
   $("#fixedmenu").css("overflow", "hidden");
-  $("#fixedmenu").animate({height: "100px"},{duration: 100});
-  $("#fixedmenu #LOGOTYPE").animate({opacity: "0"},{duration: 100});
-  $("#fixedmenu svg").animate({top: "-28px"},{duration: 100});
   $("#fixedmenu svg ").css("transform", "scale(0.8,0.8)");
+  if (isAnimated) {
+    $("#fixedmenu").animate({height: "100px"},{duration: 100});
+    $("#fixedmenu #LOGOTYPE").animate({opacity: "0"},{duration: 100});
+    $("#fixedmenu svg").animate({top: "-28px"},{duration: 100});
+  } else {
+    $("#fixedmenu").css({height: "100px"});
+    $("#fixedmenu #LOGOTYPE").css({opacity: "0"});
+    $("#fixedmenu svg").css({top: "-28px"});
+  }
   isNavDisplayed = false;
 };
 
@@ -36,22 +46,32 @@ var NV_expand = function(){
 function adjustNav(){
   
   var v_crdt = $(window).scrollTop();
-  if (v_crdt >= 100){
-    if(isTopPage){
-      NV_minimize();
-      isTopPage = false;
+  
+  if (isHome) {
+    if (v_crdt >= 100){
+      if(isTopPage){
+        NV_minimize(true);
+        isTopPage = false;
+      }
+    } else if ( v_crdt < 100 ) {
+      if(!isTopPage){
+        NV_expand();
+        isTopPage = true;
+      }
     }
-  } else if ( v_crdt < 100 ) {
-    if(!isTopPage){
-      NV_expand();
-      isTopPage = true;
-    }
+  } else {
+    NV_minimize(false);
+    isTopPage = false;
   }
 }
 
 
 // スクロール判定
 $(window).on("load scroll", adjustNav);
+
+$("#fixedmenu .svxlogobox").click(function(){
+  location.href = "http://yuyasvx.github.io/"
+});
 
 // マウスオーバー
 $("#fixedmenu").hover(
@@ -62,7 +82,7 @@ $("#fixedmenu").hover(
   },
   function () {
     if(isNavDisplayed && !isTopPage){
-      NV_minimize();
+      NV_minimize(true);
     }
   }
 );
